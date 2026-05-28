400c5218142b9be77e3cc21014afe76933313e2d
kartik kulal <kulalk788@gmail.com>
2026-05-28 21:17:58 +0530

    python for ai

---
+++ variables.py
@@ -0,0 +1,64 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+# print("Start small. Ship something.")
+# print("hello world")
+# a=10
+# print(a)
+# print("hi","my name is kartik kulal")
+# print("35"+"23")
+# name = "karik kulal"  
+# age  = 20.00
+# age = 31
+# print("my name is:",name ,"my age is:",age)
+# print(type(name))
+# print(type(age))
+
+# # a = None
+# print(a)
+
+# a=10
+# b=10
+# print(not a!=b)
+# sum=a+b
+# print(sum)
+
+# power = a**2
+# print(power)
+# print(4/2)
+
+# print(a==b)
+
+# a+=10  not allowed a++ od ++a
+# print(a)
+# a+=1
+# print(a)
+
+# print(not True)
+
+# c = "kulal"
+# d = 1.1
+# sum1 = c + str(d)  type casting  and type conversion automatically
+# print(sum1)
+
+# f = input("enter ur name:")   always of type string
+# print(f)
+
+# n = input("enter ur name:") 
+# a = int(input("enter ur age:")) type casting it to int value
+# m = float(input("enter ur marks"))
+
+# print("name of the student",n,"age:",a,"marks scored:",m)
+
+# z = int(input("enter the first number"))
+# x = float(input("enter the second number"))
+
+# print("the sum is:",z+x)
+
+# y = int(input("enter the side of square"))
+# print("the area of square:",y*y)
+
+x = float(input("enter the number1: "))
+y = float(input("enter the number2: "))
+
+print(x>=y)  #gives boolean value
+print("hello wolrd")
